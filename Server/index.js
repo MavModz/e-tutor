ab1820323e9facf23914e3495684e468343cd653
mavmodz <maverickjetman07@gmail.com>
2024-04-04 13:04:23 +0530

    updates on backend and frontend apis files for productioen environment

--- Server/index.js
+++ Server/index.js
@@ -9,12 +9,21 @@ const PORT = process.env.instance_Port;
 
 //MIDDLEWARE
 app.use(express.json());
-app.use(cors());
+
+// CORS OPTIONS TO ALLOW ALL ORIGINS
+const corsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+};
+app.use(cors(corsOptions));
+
 app.use("/", router);
 
-app.listen(PORT, () => {
-    console.log("server listening on port: " + PORT);
-});
+// app.listen(PORT, () => {
+//     console.log("server listening on port: " + PORT);
+// });
 
 // MODULE EXPORT FOR PRODUCTION SERVER
-// module.exports = app;
+module.exports = app;
